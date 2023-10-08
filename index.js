2815a4589eb586d8df93969d23bad67d1fc38149
Conrad Kambi <kambiconrad@gmail.com>
2023-10-08 17:09:25 +0300

    clickable list to display movie details

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const filmList = document.getElementById("films");
+document.addEventListener('DOMContentLoaded', () => {
+  const filmList = document.getElementById('films');
+  const movieDetails = document.getElementById('movie-details');
 
-    fetch("http://localhost:3000/films")
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.json();
-        }).then(data => {
-            // Update the HTML content with the fetched data
-            if (Array.isArray(data)) {
-                const ul = document.createElement("ul");
-                data.forEach(movie => {
-                    const li = document.createElement("li");
-                    li.textContent = movie.title;
-                    ul.appendChild(li);
-                });
-                filmList.appendChild(ul);
-            } else {
-                filmList.textContent = "Invalid JSON format. Expected an array of movies.";
-            }
-        })
-        .catch(error => {
-            console.error("Fetch error:", error);
-            filmList.textContent = "Error fetching data.";
+  // Function to display movie details
+  function displayMovieDetails(movie) {
+    movieDetails.innerHTML = `
+        <h2>${movie.title}</h2>
+        <img src="${movie.poster}" alt="${movie.title} Image" width="200" height="300">
+        <p>Runtime: ${movie.runtime}</p>
+        <p>Showtime: ${movie.showtime}</p>
+        <p>Available Tickets: ${movie.capacity - movie.tickets_sold}</p>
+    `;
+  }
+
+  fetch('http://localhost:3000/films')
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // Update the HTML content with the fetched data
+      if (Array.isArray(data)) {
+        filmList.innerHTML = '';
+
+        // Create and populate <li> elements for each movie
+        data.forEach((movie, index) => {
+          const li = document.createElement('li');
+          li.textContent = movie.title;
+          li.addEventListener('click', () => {
+            displayMovieDetails(movie);
+          });
+          filmList.appendChild(li);
         });
+      } else {
+        filmList.textContent =
+          'Invalid JSON format. Expected an array of movies.';
+      }
+    })
+    .catch((error) => {
+      console.error('Fetch error:', error);
+      filmList.textContent = 'Error fetching data.';
+    });
 });
