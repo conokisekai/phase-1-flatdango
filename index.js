9e0bbc034176bce9d3a936239aa9001b097520ea
Conrad Kambi <kambiconrad@gmail.com>
2023-10-08 21:18:28 +0300

    Server updated in real time

--- index.js
+++ index.js
@@ -28,16 +28,16 @@ document.addEventListener('DOMContentLoaded', () => {
       buyTicketButton.removeAttribute('disabled');
     }
   }
-  function buyTicket() {
-    if (currentMovie) {
-      // Check if there are available tickets
-      if (currentMovie.tickets_sold < currentMovie.capacity) {
-        // Update the sold tickets count and display
-        currentMovie.tickets_sold++;
-        displayMovieDetails(currentMovie);
-      }
-    }
-  }
+//   function buyTicket() {
+//     if (currentMovie) {
+//       // Check if there are available tickets
+//       if (currentMovie.tickets_sold < currentMovie.capacity) {
+//         // Update the sold tickets count and display
+//         currentMovie.tickets_sold++;
+//         displayMovieDetails(currentMovie);
+//       }
+//     }
+//   }
   fetch('http://localhost:3000/films')
     .then((response) => {
       if (!response.ok) {
@@ -79,4 +79,81 @@ document.addEventListener('DOMContentLoaded', () => {
       console.error('Fetch error:', error);
       movieList.innerHTML = '<li>Error fetching data.</li>';
     });
+    fetch("http://localhost:3000/films")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // Check if the data is an array of movies
+      if (Array.isArray(data) && data.length > 0) {
+        moviesData = data; // Store the movie data
+
+        // Display the details of the first movie by default
+        currentMovie = data[0];
+        displayMovieDetails(currentMovie);
+
+        // Clear any existing <li> elements
+        movieList.innerHTML = "";
+
+        // Create and populate <li> elements for each movie
+        data.forEach((movie, index) => {
+          const li = document.createElement("li");
+          li.textContent = movie.title;
+          li.addEventListener("click", () => {
+            currentMovie = movie;
+            displayMovieDetails(movie);
+          });
+          movieList.appendChild(li);
+        });
+
+        // Add a click event listener to the "Buy Ticket" button
+        buyTicketButton.addEventListener("click", buyTicket);
+      } else {
+        movieList.innerHTML =
+          "<li>Invalid JSON format. Expected an array of movies.</li>";
+      }
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error);
+      movieList.innerHTML = "<li>Error fetching data.</li>";
+    });
+  function updateTicketInfo(movie) {
+    if (movie) {
+      if (movie.tickets_sold < movie.capacity) {
+        // Increment the sold tickets count
+        movie.tickets_sold++;
+
+        // Send a PUT or POST request to update the server
+        fetch(`http://localhost:3000/films/${movie.id}`, {
+          method: "PUT", // Use 'PUT' or 'POST' depending on your server's API
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(movie),
+        })
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Network response was not ok");
+            }
+            return response.json();
+          })
+          .then((updatedMovie) => {
+            // Update the currentMovie with the updated data
+            currentMovie = updatedMovie;
+            displayMovieDetails(updatedMovie);
+          })
+          .catch((error) => {
+            console.error("Update error:", error);
+          });
+      }
+    }
+  }
+
+  // Function to handle the "Buy Ticket" button click
+  function buyTicket() {
+    updateTicketInfo(currentMovie);
+  }
 });
